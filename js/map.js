65b3d4c785f1994a7e991de448e46b891ad9a4e8
Falden <aldenfanny@gmail.com>
2017-02-16 12:38:22 +0100

    scale map

--- js/map.js
+++ js/map.js
@@ -2,7 +2,9 @@ function map() {
 
 
 var map = d3.geomap()
-    .geofile('topojson/countries/USA.json');
+    .geofile('topojson/countries/USA.json')
+    .projection(d3.geo.albersUsa)
+    .scale(800);
 
 d3.select('#map')
     .call(map.draw, map);
