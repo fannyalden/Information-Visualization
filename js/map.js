e4b3267f2a8a7d3093cfabe2728da9ff56e2d6b2
Falden <aldenfanny@gmail.com>
2017-02-21 09:50:27 +0100

    database

--- js/map.js
+++ js/map.js
@@ -1,12 +1,22 @@
 function map() {
 
 
+var margin = {top: 80, right: 20, bottom: 30, left: 20},
+    width  = parseInt(d3.select('#map').style('width')),
+    widht = widht - margin.left -margin.right,
+    mapRatio = 0.5,
+    height = (width * mapRatio) + margin.top;     
+
+
 var map = d3.geomap()
     .geofile('topojson/countries/USA.json')
     .projection(d3.geo.albersUsa)
-    .scale(800);
+    .scale(width)
+    .translate([width /2, height / 2]);
 
 d3.select('#map')
     .call(map.draw, map);
+
+
 
 }
