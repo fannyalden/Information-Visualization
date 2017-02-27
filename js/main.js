9e81f80e20a431f294a491e694fddd9889d52037
Kristin Bäck <kriba265@student.liu.se>
2017-02-27 14:40:50 +0100

    added dummydata

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var area1;
 var map1;
 
-d3.csv("data/airports.csv", function (data) {
+d3.csv("data/datan.csv", function (data1) {
 
+d3.csv("data/marchen.csv", function (data2) {
    // area1 = new area(data);
-    map1 = new map(data);
+    map1 = new map(data1, data2);
 
+});
 });
 
--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-function map(data) {
+function map(data1, data2) {
 
     var zoom = d3.behavior.zoom()
             .scaleExtent([0.5, 8])
@@ -18,7 +18,7 @@ function map(data) {
     //     return format.parse(d.time);
     // }));
 
-    var filterdData = data;
+    var filterdData = data1;
 
     //Sets the colormap
     var colors = colorbrewer.Set3[10];
@@ -41,7 +41,12 @@ function map(data) {
     var path = d3.geo.path().projection(projection);
 
     //Formats the data in a feature collection trougth geoFormat()
-    var geoData = {type: "FeatureCollection", features: geoFormat(data)};
+    var geoData = {type: "FeatureCollection", features: geoFormat(data1)};
+    //geoTrav baseras på march_2016 (dvs turer) och innehåller arrayer med namn och coordinater för origin och destination
+    var geoTrav = {type: "TravelCollection", features: geoTravel(data2)};
+
+    console.log("geoDATTTA" + geoData);
+    console.log("geoTRAVELSAKEN" + geoTrav);
 
     //Loads geo data
     d3.json("data/world-topo.json", function (error, world) {
@@ -49,11 +54,82 @@ function map(data) {
         draw(countries);
     });
 
-    //Calls the filtering function 
-    d3.select("#slider").on("input", function () {
-        filterMag(this.value, data);
+   
+    //console.log(data[1]);
+
+    // //Calls the filtering function 
+    // d3.select("#slider").on("input", function () {
+    //     filterMag(this.value, data);
+    // });
+
+/*************************   HÄR JOBBAR VI MASSOR   ************************/
+
+//hämta iata från (airports)
+//läs in origin och dest från march_2016
+//jämför origin och dest med iata
+//hämta coord för stället
+
+
+
+
+// for( var i = 0; i < data2.length-1; i++){
+//     data.push({
+//         type: 'Linestring',
+//         coordinates: [d[i].long, d.lat],
+//                      [d[i+1].long, d.lat]
+//     });
+
+
+// }
+
+
+    //console.log(data2[1]);
+   
+
+    //Lägger in origin och destination i en array-----tror vi
+    function geoTravel(array) {
+        var data = [];
+        array.map(function (d, i) {
+            //Complete the code...
+            data.push({
+                type: 'Travel',
+                origin: {name: d.ORIGIN, lon: [], lat: []},
+                dest: {name: d.DEST, lon: [], lat: []}
+                //names: [d.origin, d.dest]
+
+            });
+
+        });
+//SUCCEED HIT
+
+
+
+//DET HÄR BORDE FUNKA. VI KOMMER ÅT GEODATA I FOREACHEN NEDAN. MEN FUNKAR INTE ALLTID ATT SKRIVA I KONSOLEN PÅ FIREFOX, MAN HITTAR OM MAN LETAR I AVLUSAREN.
+    //loopa genom geoTrav
+    data.forEach(function (d){
+        //console.log(" geodata" + geoData);
+        for( var i = 0; i < geoData.length; i++){
+            if(d.features.origin.name == geoData.properties.iata[i]){
+                //get coordinates
+                d.features.origin.lon.push(111); //geoData.features[1].geometry.coordinates[0]
+                d.features.origin.lat.push(geoData.features[1].geometry.coordinates[1]);
+            }
+            else if(d.dest.name == geoData.properties.iata[i]){
+                //get coordinates
+                d.features.dest.lon.push(geoData.features[1].geometry.coordinates[0]);
+                d.features.dest.lat.push(geoData.features[1].geometry.coordinates[1]);
+            }
+    
+        }
+         
     });
 
+
+        return data;
+    }
+
+/*************************   HÄR JOBBAR VI MASSOR OVANFÖR   ************************/
+     
 
 
     //Formats the data in a feature collection
@@ -67,7 +143,7 @@ function map(data) {
                     type:'Point',
                     coordinates: [d.long, d.lat]
                 },
-                properties: d,
+                properties: d,      //behöver vi verkligen ha allt?? extra beräkningstung?
 
             });
 
@@ -88,14 +164,25 @@ function map(data) {
                 .style("fill", "lightgray")
                 .style("stroke", "white");
 
-        //draw point   
+       // console.log(geoData.features("Point", [1]));
+
+        //draw point with airports 
         var point = g.selectAll("path")
             .data(geoData.features)
             .enter().append("path")
             .attr("d", path)
-            .classed("Point", true);    
+            .classed("Point", true); 
+
+        // var route = g.selectAll("path")
+        //     .data(geoTrav.features)
+        //     .attr("class", "route")
+        //     .attr("d", path);
+
+
+
         
     };
+
 
 
     //Zoom and panning method
@@ -113,5 +200,6 @@ function map(data) {
         var elem = document.getElementById('info');
         elem.innerHTML = "Place: " + value["place"] + " / Depth: " + value["depth"] + " / Magnitude: " + value["mag"] + "&nbsp;";
     }
+
 
 }
